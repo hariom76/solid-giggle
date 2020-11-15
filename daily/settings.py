721965953e2d183e8c61f306ebb16d92b098ea63
hariom76 <hariommandal24@gmail.com>
2020-11-15 21:49:20 +0530

    jf

--- daily/settings.py
+++ daily/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0v@7(4**jaj^rsrnc=b#mx!@-h^ko88^1mh5gb&d*bt$fy2_9w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
