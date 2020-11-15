6ac5ad163d607696ea5feb67c3d7d72d481b2214
hariom76 <hariommandal24@gmail.com>
2020-11-15 22:18:37 +0530

    sjhjf

--- daily/settings.py
+++ daily/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0v@7(4**jaj^rsrnc=b#mx!@-h^ko88^1mh5gb&d*bt$fy2_9w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,5 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
 
-django_heroku.settings(locals())
